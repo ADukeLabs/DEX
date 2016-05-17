eedd50cdaf6b1fc01b9de739e6893fc31099b2fa
ADukeLabs <d.alex.duke@gmail.com>
2016-05-17 19:06:41 -0400

    Initial asp.net mvc project created in Visual Studio

---
+++ DEX/DEX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DEX.Startup))]
+namespace DEX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
