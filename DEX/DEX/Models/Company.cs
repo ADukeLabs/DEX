df26a4f2fd0ec5d92314890ec0a22d66b580bc63
ADukeLabs <d.alex.duke@gmail.com>
2016-05-17 19:27:23 -0400

    Models for Company, Contact, and Job added. Code first migrations to be added next

---
+++ DEX/DEX/Models/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEX.Models
+{
+    public class Company
+    {
+        //id
+        //Name
+        //Description
+        //Location
+        //virtual list of contacts
+        //virtual list of jobs applied at this company
+        //userId
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public List<Job> JobsApplied { get; set; }
+        public List<Contact> Contacts { get; set; }
+
+    }
+}
