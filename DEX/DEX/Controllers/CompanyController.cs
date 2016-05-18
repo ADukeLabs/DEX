12c9bf7219e390879333867d53f5ede982833597
ADukeLabs <d.alex.duke@gmail.com>
2016-05-18 13:13:20 -0400

    Company Controller and ViewModel added. Controller has Details method

---
+++ DEX/DEX/Controllers/CompanyController.cs
@@ -0,0 +1,35 @@
+﻿using DEX.Models;
+using DEX.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DEX.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Company
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: Company/Details/1
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            CompanyViewModel cvm = new CompanyViewModel();
+            cvm.Company = db.Companies.Find(id);
+            return View(cvm);
+        }
+
+
+    }
+}
---
+++ DEX/DEX/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DEX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEX.ViewModels
+{
+    public class CompanyViewModel
+    {
+        public Company Company { get; set; }
+    }
+}
