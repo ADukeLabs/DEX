ae426088d67dc4f609a41a033a8ef5b61e9ebdae
ADukeLabs <d.alex.duke@gmail.com>
2016-06-18 14:11:51 -0400

    GetCompanies function added to JSFunctions

--- DEX/DEX/Scripts/JSFunction_Company.js
+++ DEX/DEX/Scripts/JSFunction_Company.js
@@ -1,7 +1,13 @@
 ﻿$(document).ready(function () {
     
+    function MakeCompanies(company) {
+        $("#companies").append(
+            '<li id="'+ company.Id + '">' +
+            '<button id="'+ company.Id + '"class="companyDetails btn btn-primary>"'+ company.Name + '"</button>'
+        );
+    }
 
     $(".GetCompanies").on("click", function () {
-        alert("button working");
+        
     });
 });
