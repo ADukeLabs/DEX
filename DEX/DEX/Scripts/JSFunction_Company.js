d8cdc43a1bce8d11d477b14f45ebf30aa2a6f218
ADukeLabs <d.alex.duke@gmail.com>
2016-07-09 11:14:28 -0400

    JavaScript views updated. Buttons displaying. Trying to make html append and remove on specific button clicks.

--- DEX/DEX/Scripts/JSFunction_Company.js
+++ DEX/DEX/Scripts/JSFunction_Company.js
@@ -1,35 +1,36 @@
 ﻿$(document).ready(function () {
-    
-    function makeCompanies(companies, Id) {
 
-        var html = '<li>' +
-                        '<button id="' + companies.Id + '" class="companyDetails btn btn-primary>"' + companies.Name + '"</button>' +
-                   '</li>';
-        $('#' + Id).append(html);
-    }
+    function makeCompanies(companies, id) {
+        var html = '<button id="' + companies.Id + '"class="company btn btn-primary">"' + companies.Name + '"</button>';
+
+        $(this).append(html);
+    };
 
 
 
 
-    $(".GetCompanies").click(function () {
-
-        var activeCity = this.id;
-        var url = "http://localhost:58905/Company/GetCompanies/";
-
-        $.ajax({
-            type: "GET",
-            url: url,
-            data: { 'id': activeCity },
-            dataType: "json",
-            success: function (companies) {
-                for (var i = 0; i < companies.length; i++) {
-                    makeCompanies(companies, activeCity);
+    $(".GetCompanies").click(function(){
+            var activeCity = $(this).parent().attr("Id");
+            var url = "http://localhost:58905/Company/GetCompanies/";
+            $.ajax({
+                type: "GET",
+                url: url,
+                data: { 'id': activeCity },
+                dataType: "json",
+                success: function (companies) {
+                    for (var i = 0; i < companies.length; i++) {
+                        makeCompanies(companies, activeCity);
+                    }
+                },
+                error: function () {
+                    alert: ("Error: There was a problem listing the companies in this city.");
                 }
-            },
-            error: function () {
-                alert: ("Error: There was a problem listing the companies in this city.");
-            }
-        });
+            });
     });
+
+
+    
+
+
 
 });
