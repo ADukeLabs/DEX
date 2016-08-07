c2cf404c484b0680a289c85c87370144f3ba98b7
ADukeLabs <d.alex.duke@gmail.com>
2016-08-06 23:53:04 -0400

    Collapse feature working, but happens on all buttons. Needs to happen when single button is clicked

--- DEX/DEX/Scripts/Company.js
+++ DEX/DEX/Scripts/Company.js
@@ -3,26 +3,13 @@
     var menuButtons = $("button.GetCompanies")
 
     menuButtons.each(function () {
-        //menuButtons.addClass("accordian");
         var cityId = $(this).parent().attr("Id");
         GetCompanyAjax(cityId);
     });
-
-    //$(function () {
-    //    $(".city-div").accordian({
-    //        collapsible: true
-    //    });
-    //});
-    
-
-
 
     //$(".GetCompanies").on("click", function() {
     //    var activeCity = $(this).parent().attr("Id");
     //    GetCompanyAjax(activeCity);
     //});
-
-    //$(".GetCompanies").on("click", function () {
-    //    $("div .company-div").remove();
-    //});    
+    
 });
