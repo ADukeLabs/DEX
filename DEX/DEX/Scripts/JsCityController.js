29388d3c1ada51a9c94fe2dee7c79d5194582526
ADukeLabs <d.alex.duke@gmail.com>
2016-08-06 20:49:52 -0400

    Javascript refactored: listcompanies and getcompanies functions now in separate file. Ready for pull request for alternative frontend feature branch

---
+++ DEX/DEX/Scripts/JsCityController.js
@@ -0,0 +1,25 @@
+﻿function CompanyList(company, cityId) {
+    $("#" + cityId).append('<div id="' + JSON.parse(company.Id) + '"class="company-div col-xs-10 col-sm-6 col-sm-offset-4">' +
+                                '<button type="button" class="company-button btn btn-primary">' + company.Name + '</button>' +
+                            '</div>');
+};
+
+
+function GetCompanyAjax(city) {
+    var url = "../Company/GetCompanies/";
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        data: { 'id': city },
+        dataType: "json",
+        success: function (companies) {
+            for (var i = 0; i < companies.length; i++) {
+                CompanyList(companies[i], city);
+            }
+        },
+        error: function () {
+            alert: ("Error: There was a problem listing the companies in this city.");
+        }
+    });
+};
