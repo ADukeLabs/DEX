a8ce781534eb3b674e8f6bddc0033603968a5005
ADukeLabs <d.alex.duke@gmail.com>
2016-08-26 17:03:58 -0400

    Company object transfered to Company.js file. Ready for pull request to merge into master.

--- DEX/DEX/Scripts/MenuController.js
+++ DEX/DEX/Scripts/MenuController.js
@@ -1,17 +1,7 @@
-﻿function Company(company) {
-    this.Id = JSON.parse(company.Id);
-    this.Name = company.Name;
-    //this.Address = JSON.parse(company.Address);
-    //this.Industry = JSON.parse(company.Industry);
-    //this.City = JSON.parse(data.City);
-    //this.contacts = JSON.parse(data.contacts);
-}
-
-function CompanyList(company, cityId) {
-    var data = new Company(company);
-    console.log(data.Name);
-    $("#" + cityId).append('<div id="' + data.Id + '"class="company-div col-xs-10 col-sm-6 col-sm-offset-4">' +
-                                '<button type="button" class="company-button btn btn-primary">' + data.Name + '</button>' +
+﻿function CompanyList(companyData, cityId) {
+    var company = new Company(companyData);
+    $("#" + cityId).append('<div id="' + company.Id + '"class="company-div col-xs-10 col-sm-6 col-sm-offset-4">' +
+                                '<button type="button" class="company-button btn btn-primary">' + company.Name + '</button>' +
                             '</div>');
 };
 
