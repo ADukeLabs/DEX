73848b3704cc7cf782e8b9848215efe0d0675574
ADukeLabs <d.alex.duke@gmail.com>
2016-09-03 13:01:26 -0400

    Company button working. Able to grab Id from parent div. Ready for dynamic company page feature.

--- DEX/DEX/Scripts/Menu.js
+++ DEX/DEX/Scripts/Menu.js
@@ -2,19 +2,21 @@
     
     $("#content-box").fadeIn("slow");
 
-    $(".city-button").on("click", function () {
+
+
+    $(".city-button").on('click', function () {
         var activeCity = $(this).parent().attr("Id");
         GetCompanyAjax(activeCity);
-        $(".company-button").fadeIn("slow");
+        $(".company-div").fadeIn("slow");
     });
 
-    $(this).on("click", function () {
+    $(".city-button").click(function () {
         $(".company-div").remove();
-    });
+    });                                                                      
 
-    //$(".company-button").on("click", function () {
-    //    console.log("this button is working");
-    //    //$(".city-div").fadeOut("slow");
-    //});
+    $("body").on("click", ".company-button", function () {
+        var id = $(this).parent().attr("Id");
+        console.log(id);
+    });
 
 });
