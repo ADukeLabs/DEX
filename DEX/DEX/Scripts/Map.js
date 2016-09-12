97d718c1cbd423892e11ec3fdc644655f8a1af64
ADukeLabs <d.alex.duke@gmail.com>
2016-09-12 09:58:15 -0400

    Google map feature not working. On load, receiving error: Cannot read property offsetWidth of null. Maybe due to map div being dynamic element. Pull request for help.

---
+++ DEX/DEX/Scripts/Map.js
@@ -0,0 +1,10 @@
+﻿function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 4,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("#map"), mapOptions);
+}
+
+google.maps.event.addDomListener(window, "load", initialize);
--- DEX/DEX/Scripts/MenuController.js
+++ DEX/DEX/Scripts/MenuController.js
@@ -1,7 +1,7 @@
 ﻿function CompanyList(companyData, cityId) {
     var company = new Company(companyData);
     $("#" + cityId).append('<div id="' + company.Id + '"class="company-div col-xs-10 col-sm-6 col-sm-offset-4">' +
-                                '<button type="button" class="company-button btn btn-primary">' + company.Name + '</button>' +
+                                '<button type="button" class="company-button btn btn-primary col-xs-6">' + company.Name + '</button>' +
                             '</div>');
 };
 
@@ -11,8 +11,8 @@ function companyView(data, companyId)
     var html = '<div id="' + company.Id + '" class="well col-xs-12 col-sm-12">' +
                     '<h2 class="company-heading col-sm-10 col-sm-offset-2">' + company.Name + '</h2>' +
                     '<p class="col-xs-10 col-sm-10 col-sm-offset-2">' + company.Address + '</p>' +
-                    '<div id="mapDiv"></div>'+
-                    '<button class="btn btn-primary col-xs-2 col-sm-offset-2">View Contacts</button>' +
+                    '<div id="map" class="col-xs-10 col-sm-10 col-sm-offset-2"></div>' +
+                    '<button class="btn btn-primary col-xs-6 col-sm-2 col-sm-offset-2">View Contacts</button>' +
                     //'<button class="btn btn-primary col-xs-2 col-sm-offset-2">Edit Company</button>' +
                     //'<button class="btn btn-danger col-xs-2 col-sm-offset-2">Delete Company</button>' +
                '</div>';
