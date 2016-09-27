0f7745f5025f72b02e604c638cba6b5113d6ec4f
ADukeLabs <d.alex.duke@gmail.com>
2016-09-27 11:11:23 -0400

    Getting Contact object from array in Company object. Cannot parse into Contact JS object in ContactList method. Needs refactoring.

--- DEX/DEX/Scripts/MenuController.js
+++ DEX/DEX/Scripts/MenuController.js
@@ -1,6 +1,6 @@
 ﻿function CompanyList(companyData, cityId) {
     var company = new Company(companyData);
-    $("#" + cityId).append('<div id="' + company.Id + '"class="company-div col-xs-10 col-sm-6 col-sm-offset-4">' +
+    $("#" + cityId).append('<div id="' + company.Id + '"class="company-div col-xs-10 col-xs-offset-2 col-sm-6 col-sm-offset-4">' +
                                 '<button type="button" class="company-button btn btn-primary">' + company.Name + '</button>' +
                             '</div>');
 };
@@ -11,19 +11,18 @@ function companyView(data, companyId)
     var html = '<div id="' + company.Id + '" class="well col-xs-12 col-sm-12">' +
                     '<h2 class="company-heading col-sm-10 col-sm-offset-2">' + company.Name + '</h2>' +
                     '<p class="col-xs-10 col-sm-10 col-sm-offset-2">' + company.Address + '</p>' +
-                    //$.each(company.Contacts, function (i) { $(this).append(contactList(i)) });
-                    $.each(company.contacts, contactList(contact));
+                    
                '</div>';
+    $.each(company.Contacts, function (i) { contactList(company.Contacts[i])});
     $("#content-box").append(html);
 }
 
 function contactList(data) {
     var contact = new Contact(data);
-    var html = '<div id="'+contact.Id+'" class="contact-container">' +
+    var html = '<div class="contact-container">' +
                     '<h3 class="col-xs-12 col-sm-10 col-sm-offset-2">' + contact.Name + '</h3>' +
                     '<p class="col-xs-12 col-sm-10 col-sm-offset-2">' + contact.Title + '</p>' +
                     //'<p class="col-xs-6">' + contact.Email + '</p>' +
                 '</div>';
-    $(this).append(html);
-    //console.log(contact.Name, contact.Title);
+    $(".well").append(html);
 }
