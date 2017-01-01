607f3b0591f9b86ca819c3e7d05d8a4f765bf4cd
ADukeLabs <d.alex.duke@gmail.com>
2017-01-02 12:38:58 +1300

    Logout action now redirects to Login Page. User can now create own account and personalize experience. Ready for merge to master for new feature.

--- DEX/DEX/Controllers/AccountController.cs
+++ DEX/DEX/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace DEX.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
