5467b2640cad8d0ee6b3a7ae586674c0c4a1eee5
ADukeLabs <d.alex.duke@gmail.com>
2017-11-01 17:50:55 +1300

    Company Repository methods for get all, get company, and create company implemented.

--- DEX/DEX_Api/Models/City.cs
+++ DEX/DEX_Api/Models/City.cs
@@ -9,5 +9,10 @@ namespace DEX_Api.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public City(string name)
+        {
+            Name = name;
+        }
     }
 }
