cb75ddbdb54dd7e2be01f2b3d212145d85a12e94
ADukeLabs <d.alex.duke@gmail.com>
2018-06-04 19:11:51 +1200

    Updates
    Updates to repository: new version of boostratp updated. JQuery removed for React replacing. Updates to Popper js plugin.

--- DEX/DEX/Scripts/React/HomeMenu.jsx
+++ DEX/DEX/Scripts/React/HomeMenu.jsx
@@ -1,7 +1,14 @@
 ﻿class Menu extends React.Component {
+    Render() {
+        return <div className="city-div">
+                   <button className='city-button btn btn-primary btn-block' type="button"></button>
+               </div>;
+    }
 
 }
 
 class App extends React.Component {
 
 }
+
+const render = ReactDOM.Render(<App></App>);
