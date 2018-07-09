51a7684a2d7bd5c2b9bd2d35e9f1d250703302cc
ADukeLabs <d.alex.duke@gmail.com>
2018-07-09 22:05:44 +1200

    React Links Working
    Links imported into _Layout to enable React and React DOM in project.

--- DEX/DEX/Scripts/Company.js
+++ DEX/DEX/Scripts/Company.js
@@ -1,7 +1,7 @@
-﻿function Company(data) {
-    this.Id = JSON.parse(data.Id);
-    this.Name = data.Name;
-    this.Address = data.Address;
-    this.City = data.City;
-    this.Contacts = data.Contacts;
-}
+﻿//function Company(data) {
+//    this.Id = JSON.parse(data.Id);
+//    this.Name = data.Name;
+//    this.Address = data.Address;
+//    this.City = data.City;
+//    this.Contacts = data.Contacts;
+//}
--- DEX/DEX/Scripts/Menu.js
+++ DEX/DEX/Scripts/Menu.js
@@ -1,21 +1,21 @@
-﻿$(document).ready(function () {
+﻿//$(document).ready(function () {
     
-    $("#content-box").fadeIn("slow");
+//    $("#content-box").fadeIn("slow");
 
-    $(".city-button").on('click', function () {
-        var activeCity = $(this).parent().attr("Id");
-        GetCityCompanies(activeCity);
-        $(".company-div").fadeIn("slow");
-    });
+//    $(".city-button").on('click', function () {
+//        var activeCity = $(this).parent().attr("Id");
+//        GetCityCompanies(activeCity);
+//        $(".company-div").fadeIn("slow");
+//    });
 
-    $(".city-button").click(function () {
-        $(".company-div").remove();
-    });                                                                      
+//    $(".city-button").click(function () {
+//        $(".company-div").remove();
+//    });                                                                      
 
-    $("#content-box").on("click", ".company-button", function () {
-        var id = $(this).parent().attr("Id");
-        $(".city-div").fadeOut("slow");
-        GetCompanyDetails(id);
-    });
+//    $("#content-box").on("click", ".company-button", function () {
+//        var id = $(this).parent().attr("Id");
+//        $(".city-div").fadeOut("slow");
+//        GetCompanyDetails(id);
+//    });
 
-});
+//});
