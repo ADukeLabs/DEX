a236e9506043a1697281ac6c973d2e3172bdba53
ADukeLabs <d.alex.duke@gmail.com>
2018-12-30 19:34:48 +1300

    ListCompanies function added
    Created a function to list companies associated with a particular city. Only using static data in an array for now. Buttons with company names not rendering.

--- DEX/Dex_Client/Scripts/React/HomeMenu.jsx
+++ DEX/Dex_Client/Scripts/React/HomeMenu.jsx
@@ -1,14 +1,34 @@
-﻿const CityButton = (props) => {
-    return (
-        <div className="city-div">
-            <button className='city-button btn btn-primary btn-block' type="button">{props.city.name}</button>
+﻿//const CityButton = (props) => {
+//    return (
+//        <div className="city-div">
+//            <button className='city-button btn btn-primary btn-block' type="button">{props.city.name}</button>
+//        </div>
+//    );
+//}
+
+const data = [{ "Name": "Datacom" }, { "Name": "Intergen" }, { "Name": "MBIE" }, { "Name": "Xero" }];
+
+function ListCompanies(props) {
+    const listItems = this.props.data.map((c) =>
+        <div className="col-xs-10 col-xs-offset-2 col-sm-6 col-sm-offset-4">
+            <button type="button" className="btn btn-primary">c.Name</button>
         </div>
     );
+    render() {
+        return (listItems);
+    }
 }
 
-//const CityButtonList = (props) => {
 
-//}
+class CityButton extends React.Component {
+    render() {
+        return (
+            <div className="city-div">
+                <button className='city-button btn btn-primary btn-block' type="button" onClick={ListCompanies(data)}>Wellington, New Zealand</button>
+            </div>
+        );
+    }
+}                                   
 
 
 class Menu extends React.Component {
