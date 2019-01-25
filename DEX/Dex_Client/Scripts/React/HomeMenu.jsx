d4f67893aeda4c2fcebd16b49af31198a86fbabf
ADukeLabs <d.alex.duke@gmail.com>
2019-01-26 10:25:50 +1300

    Update HomeMenu.jsx
    Commit of latest changes in preparation for new branch.

--- DEX/Dex_Client/Scripts/React/HomeMenu.jsx
+++ DEX/Dex_Client/Scripts/React/HomeMenu.jsx
@@ -3,7 +3,6 @@
     constructor(props)
     {
         super(props);
-        data = [{ "Name": "Datacom" }];
         this.state = { isButtonOn: false };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -13,34 +12,37 @@
         this.setState({ isButtonOn: !this.state.isButtonOn });
         console.log(this.state.isButtonOn);
     }
- 
     render() {
         return (
             <div className="city-div">
                 <button className='city-button btn btn-primary btn-block' type="button" onClick={this.handleClick}>Wellington, New Zealand</button>
+                <CompanyList />
             </div>
         );
     }          
 }
 
-//class CompanyList extends React.Component(props) {
-//    listItems = this.props.data;
-//    render() {
-//        return (
-//            listItems.map((c) =>
-//                <div className="col-xs-10 col-xs-offset-2 col-sm-6 col-sm-offset-4">
-//                    <button type="button" className="btn btn-primary">c.Name</button>
-//                </div>
-//            )   
-//        );
-//    }
-//}
+class CompanyList extends React.Component() {
+
+    render() {
+        if (this.props.state.isButtonOn == true) {
+            return (
+                data.map((c) =>
+                    <div className="col-xs-10 col-xs-offset-2 col-sm-6 col-sm-offset-4">
+                        <button type="button" className="btn btn-primary">c.Name</button>
+                    </div>
+                )
+            );
+        }
+        
+    }
+}
 
 
 class Menu extends React.Component {
     render() {
         return (
-            <CityButton  />
+            <CityButton />
         );
     }
 }
