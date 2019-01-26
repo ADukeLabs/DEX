aa789f70220cc8e497dd5214eaffd310a3bb05a4
ADukeLabs <d.alex.duke@gmail.com>
2019-01-26 13:32:37 +1300

    React Re-Installed
    ReactJS and JSX configurations re-installed. Ready for merge into previous branch.

--- DEX/Dex_Client/App_Start/ReactConfig.cs
+++ DEX/Dex_Client/App_Start/ReactConfig.cs
@@ -1,4 +1,6 @@
 using React;
+using JavaScriptEngineSwitcher.Core;
+using JavaScriptEngineSwitcher.V8;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Dex_Client.ReactConfig), "Configure")]
 
@@ -12,10 +14,10 @@ namespace Dex_Client
 			// add all the necessary JavaScript files here. This includes 
 			// your components as well as all of their dependencies.
 			// See http://reactjs.net/ for more information. Example:
-			//ReactSiteConfiguration.Configuration
+			ReactSiteConfiguration.Configuration
 			//	.AddScript("~/Scripts/First.jsx")
 			//	.AddScript("~/Scripts/Second.jsx");
-			
+			.AddScript("~/Scripts/React.HomeMenu.jsx");
 			// If you use an external build too (for example, Babel, Webpack,
 			// Browserify or Gulp), you can improve performance by disabling 
 			// ReactJS.NET's version of Babel and loading the pre-transpiled 
